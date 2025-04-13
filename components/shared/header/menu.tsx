ee6b6a105b53796a70b58068af7838ab013d6fe8
zoltan-molnar <rdigital4455@gmail.com>
2025-04-13 23:16:53 +0200

    2:20

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,19 +1,50 @@
-import { ShoppingCartIcon, UserIcon } from "lucide-react";
-import Link from "next/link";
+import { EllipsisVertical } from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import CartButton from './cart-button'
 
-export default function Menu() {
+import ThemeSwitcher from './theme-switcher'
+import LanguageSwitcher from './language-switcher'
+import { useTranslations } from 'next-intl'
+import UserButton from './user-button'
+
+const Menu = ({ forAdmin = false }: { forAdmin?: boolean }) => {
+  const t = useTranslations()
   return (
-    <div className="flex justify-end px-4">
-      <nav className="flex gap-6 items-center">
-        <Link href="/user" className="flex items-center gap-2">
-          <UserIcon className="h-6 w-6" />
-          <span className='font-bold'>Sign In</span>
-        </Link>
-        <Link href="/cart" className="flex items-center gap-2">
-          <ShoppingCartIcon className='h-6 w-6'/>
-          <span className='font-bold'>Cart</span>
-        </Link>
+    <div className='flex justify-end'>
+      <nav className='md:flex gap-3 hidden w-full'>
+        <LanguageSwitcher />
+        <ThemeSwitcher />
+        <UserButton />
+        {forAdmin ? null : <CartButton />}
+      </nav>
+      <nav className='md:hidden'>
+        <Sheet>
+          <SheetTrigger className='align-middle header-button'>
+            <EllipsisVertical className='h-6 w-6' />
+          </SheetTrigger>
+          <SheetContent className='bg-black text-white  flex flex-col items-start  '>
+            <SheetHeader className='w-full'>
+              <div className='flex items-center justify-between '>
+                <SheetTitle className='  '>{t('Header.Site Menu')}</SheetTitle>
+                <SheetDescription></SheetDescription>
+              </div>
+            </SheetHeader>
+            <LanguageSwitcher />
+            <ThemeSwitcher />
+            <UserButton />
+            <CartButton />
+          </SheetContent>
+        </Sheet>
       </nav>
     </div>
-  );
+  )
 }
+
+export default Menu
